5a70be4c92a80d4a1c749cd067458cd17e740a4a
SheeleM <41965065+SheeleM@users.noreply.github.com>
2024-05-20 23:19:35 -0500

    entrega 3

---
+++ src/app/registro-arbitro/registro-arbitro.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-arbitro',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro-arbitro.component.html',
+  styleUrl: './registro-arbitro.component.css'
+})
+export class RegistroArbitroComponent {
+  constructor(){
+  }
+  nombre='sulu';
+}
---
+++ src/app/registro-jugador/registro-jugador.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-jugador',
+  standalone: true,
+  imports: [],
+  templateUrl: './registro-jugador.component.html',
+  styleUrl: './registro-jugador.component.css'
+})
+export class RegistroJugadorComponent {
+
+}
